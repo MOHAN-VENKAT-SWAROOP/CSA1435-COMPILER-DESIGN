e1c045ac653413e63c9132ee9574cd38c066f97f
MOHAN-VENKAT-SWAROOP <madabathulaswaroop0134.sse@saveetha.com>
2023-10-19 10:28:41 +0530

    commit

---
+++ C PROGRAMS/EXP-39.to recognize operator +,-,,/recognize operator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+
+    // User input for the operator
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            printf("You entered addition operator (+)\n");
+            break;
+        case '-':
+            printf("You entered subtraction operator (-)\n");
+            break;
+        case '*':
+            printf("You entered multiplication operator (*)\n");
+            break;
+        case '/':
+            printf("You entered division operator (/)\n");
+            break;
+        default:
+            printf("Invalid operator\n");
+            break;
+    }
+
+    return 0;
+}
